6f433aa837f73f35b723c195ef8b2639a8e091c3
yashar.xaspoladov <yashar.xaspoladov@gmail.com>
2026-03-08 15:32:20 +0400

    make the accountIds list on auditEntry readonly

--- BankingService/Domain/AuditEntry.cs
+++ BankingService/Domain/AuditEntry.cs
@@ -3,7 +3,7 @@ namespace BankingService.Domain;
 public class AuditEntry : Entity
 {
     public required Operation Operation   { get; init; }
-    public required List<Guid> AccountIds { get; init; }
+    public required IReadOnlyList<Guid> AccountIds { get; init; }
     public required Money Amount          { get; init; }
     public required Guid IdempotencyKey   { get; init; }
     public required bool IsSuccess        { get; init; }
